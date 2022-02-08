7b6ab1a80738bdf9b1a2b694c815cd0de83b9a46
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2022-02-08 08:24:04 -0300

    feat: S3 confi, endpoint /upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # s3 server
+para cargar los datos primero cambie el nombre de .initial.env por .env y complete los datos por los datos de acceso a s3 configurado en aws IAM access (creado una politica y luego un usuario para usar dicha politicia). Luego de esto puede configurar un PORT en el .env.
+ponga a funcionar la aplicacion
+```bash
+npm run dev
+```
+
+luego de esto puede mandar archivos por medio de un formulario multipart fomr data y pasarlos por un file input llamado file.
+luego de esto obtendra una respusta con la informacion de carga del archivo.
+en un futuro ampliare esta documentacion.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,63 @@
-const express = require('express');
+var aws = require('aws-sdk')
+var express = require('express')
+var multer = require('multer')
+var multerS3 = require('multer-s3')
+const crypto = require("crypto");
+require('dotenv').config();
 
-const app = express();
+var app = express()
+/**
+ * cofiguraciones de acceso a aws.
+ */
+var s3 = new aws.S3({
+    region: process.env.AWS_BUCKET_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
+});
 
-app.use(require('cors'));
+/**
+ * configuracion de carga de archivos a s3
+ */
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null,{
 
-app.listen(process.env.PORT || 3000, ()=>{
+      });
+    },
+    key: function (req, file, cb) {
+      cb(null, encriptfile(file.originalname));
+    }
+  })
+}).single('file');
+
+/**
+ * esta funcion retorna el nombre encritado de un archivo.
+ * @param {String} filename nombre original del archivo 
+ * @returns nombre encriptado
+ */
+function encriptfile(filename) {
+  return (
+      crypto.createHash("sha1").update(filename).digest("hex") +
+      "." +
+      filename.split(".")[1]
+  );
+}
+
+/**
+ * funcion que expone un endpoint post para la carga del archivo.
+ */
+app.post('/upload', async(req, res)=>{
+  upload(req, res, async(err)=>{
+    if(err) console.log(err);
+    console.log(req.file);
+    res.send(req.file);
+  })
+})
+
+app.listen(process.env.PORT || 3000, () => {
     console.log(`listen on port ${process.env.PORT || 3000}`);
 })
